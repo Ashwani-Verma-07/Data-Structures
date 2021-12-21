398db82d67347abb45ba734a0c7c94f671fd69b8
Ashwani Verma <89683890+Ashwani-Verma-07@users.noreply.github.com>
2021-12-21 18:46:48 +0530

    pair_inside_pair

--- Stacks/templates.cpp
+++ Stacks/templates.cpp
@@ -1,14 +1,40 @@
 #include <iostream>
 using namespace std;
-template <typename T>
-class Pair
-{        //                   x    y
-    T x; // int x;         -----------
-         //                 |int |int|
-         //                -----------
-    T y; //int y;               p1
+template <typename T, typename V>
+// class Pair
+// {        //                   x    y
+//     T x; // int x;         -----------
+//          //                 |int |int|
+//          //                -----------
+//     T y; //int y;               p1
 
-    //                      Pair<int,int>p1;
+//     //                      Pair<int,int>p1;
+// public:
+//     void setX(T x) //void setX(int x)
+//     {
+//         this->x = x;
+//     }
+//     T getX() //int getX;
+//     {
+//         return x;
+//     }
+//     void setY(T y) //void setY(int y)
+//     {
+//         this->y = y;
+//     }
+//     T getY() //int getY
+//     {
+//         return y;
+//     }
+// };
+class Pair ///////////////////// x        y
+{          //                   x,y       y
+    T x;   // int x;         ------------------
+           //                 ||int,int| int|
+           //                ------------------
+    V y;   //int y;               p1
+
+    //                      Pair<int,double>p1;
 public:
     void setX(T x) //void setX(int x)
     {
@@ -18,23 +44,26 @@ public:
     {
         return x;
     }
-    void setY(T y) //void setY(int y)
+    void setY(V y) //void setY(int y)
     {
         this->y = y;
     }
-    T getY() //int getY
+    V getY() //int getY
     {
         return y;
     }
 };
 int main()
 {
-    Pair<int> p1; //both x and y are int datatype
-    p1.setX(10);
+    Pair<Pair<int, int>, int> p1;
+    // Pair<int, double> p1; //both x and y are int datatype and double datatype.
+    // p1.setX(10);
+    // p1.setY(20.234);
+    // cout << p1.getX() << " " << p1.getY() << endl;
+    Pair<int, int> p2;
+    p2.setX(10);
+    p2.setY(15);
+    p1.setX(p2);
     p1.setY(20);
-    cout << p1.getX() << " " << p1.getY() << endl;
-    Pair<double> p2; //both x and y are double datatype
-    p2.setX(10.24);
-    p2.setY(20.56);
-    cout << p2.getX() << " " << p2.getY() << endl;
+    cout << p1.getX().getX() << " " << p1.getX().getY() << " " << p1.getY() << endl;
 }
