5d50caa50c3928dd249650d84647b9bead4df03d
Ashwani Verma <89683890+Ashwani-Verma-07@users.noreply.github.com>
2022-01-22 16:04:03 +0530

    left_clone

---
+++ Binary_Trees/Transform_to_left_clone_Tree.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+using namespace std;
+class BinaryTree
+{
+public:
+    int data;
+    BinaryTree *left;
+    BinaryTree *right;
+    BinaryTree(int data)
+    {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+int max_so_far = INT_MIN;
+int min_so_far = INT_MAX;
+
+BinaryTree *input()
+{
+    int rootData;
+    cin >> rootData;
+    BinaryTree *root = new BinaryTree(rootData);
+    queue<BinaryTree *> Queue;
+    Queue.push(root);
+    while (Queue.size() != 0)
+    {
+        int leftData, rightData;
+        BinaryTree *front = Queue.front();
+        Queue.pop();
+        cout << "Enter left Data of " << front->data << endl;
+        cin >> leftData;
+        if (leftData != -1)
+        {
+            BinaryTree *child = new BinaryTree(leftData);
+            front->left = child;
+            Queue.push(child);
+        }
+        cout << "Enter right Data of " << front->data << endl;
+        cin >> rightData;
+        if (rightData != -1)
+        {
+            BinaryTree *child = new BinaryTree(rightData);
+            front->right = child;
+            Queue.push(child);
+        }
+    }
+    return root;
+}
+BinaryTree *leftcTree(BinaryTree *root)
+{
+    if (root == NULL)
+    {
+        return NULL;
+    }
+    BinaryTree *lcr = leftcTree(root->left);
+    BinaryTree *rcr = leftcTree(root->right);
+    BinaryTree *newNode = new BinaryTree(root->data);
+    newNode->left = lcr;
+    newNode->right = NULL;
+    root->left = newNode;
+    root->right = rcr;
+    return root;
+}
+
+void output(BinaryTree *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    queue<BinaryTree *> Queue_;
+    Queue_.push(root);
+    while (Queue_.size() != 0)
+    {
+        BinaryTree *front = Queue_.front();
+        Queue_.pop();
+        cout << front->data << " :";
+        if (front->left != NULL)
+        {
+            cout << " L->" << front->left->data;
+            Queue_.push(front->left);
+        }
+        if (front->right != NULL)
+        {
+            cout << " R->" << front->right->data;
+            Queue_.push(front->right);
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    BinaryTree *root = input();
+    output(root);
+    cout << endl;
+    BinaryTree *index_ = leftcTree(root);
+    cout << "Transform to left cloned TREE" << endl;
+    output(index_);
+}
+//1 2 4 9 -1 5 6 -1 -1 7 10 -1 3 8 -1 -1 -1 -1 -1 -1 -1
