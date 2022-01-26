de73c81e9a0e8799dc03085e3e90b575b14b27c3
Ashwani Verma <89683890+Ashwani-Verma-07@users.noreply.github.com>
2022-01-26 18:17:33 +0530

    Flatten to right node

---
+++ Binary_Trees/flatten_bt.cpp
@@ -0,0 +1,124 @@
+#include <bits/stdc++.h>
+using namespace std;
+class BinaryTree
+{
+public:
+    int data;
+    BinaryTree *left;
+    BinaryTree *right;
+    BinaryTree(int data)
+    {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+BinaryTree *input()
+{
+    int rootData;
+    cin >> rootData;
+    BinaryTree *root = new BinaryTree(rootData);
+    queue<BinaryTree *> Queue;
+    Queue.push(root);
+    while (Queue.size() != 0)
+    {
+        int leftData, rightData;
+        BinaryTree *front = Queue.front();
+        Queue.pop();
+        cout << "Enter left Data of " << front->data << endl;
+        cin >> leftData;
+        if (leftData != -1)
+        {
+            BinaryTree *child = new BinaryTree(leftData);
+            front->left = child;
+            Queue.push(child);
+        }
+        cout << "Enter right Data of " << front->data << endl;
+        cin >> rightData;
+        if (rightData != -1)
+        {
+            BinaryTree *child = new BinaryTree(rightData);
+            front->right = child;
+            Queue.push(child);
+        }
+    }
+    return root;
+}
+void flatten(BinaryTree *root)
+{
+    if (root == NULL || (root->left == NULL && root->right == NULL))
+    {
+        return;
+    }
+    if (root->left != NULL)
+    {
+        flatten(root->left);
+        BinaryTree *temp = root->right;
+        root->right = root->left;
+        root->left = NULL;
+        BinaryTree *t = root->right;
+        while (t->right != NULL)
+        {
+            t = t->right;
+        }
+        t->right = temp;
+    }
+    flatten(root->right);
+}
+void output(BinaryTree *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    queue<BinaryTree *> Queue_;
+    Queue_.push(root);
+    while (Queue_.size() != 0)
+    {
+        BinaryTree *front = Queue_.front();
+        Queue_.pop();
+        cout << front->data << " :";
+        if (front->left != NULL)
+        {
+            cout << " L->" << front->left->data;
+            Queue_.push(front->left);
+        }
+        if (front->right != NULL)
+        {
+            cout << " R->" << front->right->data;
+            Queue_.push(front->right);
+        }
+        cout << endl;
+    }
+}
+void inorder(BinaryTree *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+int main()
+{
+    BinaryTree *root = input();
+    output(root);
+    flatten(root);
+    output(root);
+    inorder(root);
+    cout << endl;
+}
+//1 2 4 9 -1 5 6 -1 -1 7 10 -1 3 8 -1 -1 -1 -1 -1 -1 -1
