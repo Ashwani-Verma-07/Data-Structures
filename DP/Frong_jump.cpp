03929da7fd99da6fa644ccbb4f2447c9dddf9a50
Ashwani Verma <89683890+Ashwani-Verma-07@users.noreply.github.com>
2022-06-09 19:38:18 +0530

    Frog_jump

--- DP/Frong_jump.cpp
+++ DP/Frong_jump.cpp
@@ -1,19 +1,57 @@
 #include <iostream>
+#include <vector>
+#include <climits>
 using namespace std;
 // Frog can jump 1 or 2 indexes | get minimum energy used to go from 0 to n-1
-int frog_jump(int num[], int n)
+int frog_jump(int num[], int n, vector<int> &dp)
 {
     if (n == 0)
-    {
         return 0;
-    }
-    int left = frog_jump(num, n - 1) + abs(num[n - 1] - num[n]);
+
+    if (dp[n] != -1)
+        return dp[n];
+    int left = frog_jump(num, n - 1, dp) + abs(num[n] - num[n - 1]);
     int right;
     if (n > 1)
     {
-        right = frog_jump(num, n - 2) + abs(num[n - 2] - num[n]);
+        right = frog_jump(num, n - 2, dp) + abs(num[n] - num[n - 2]);
     }
-    return min(left, right);
+    return dp[n] = min(left, right);
+}
+// Tabulation
+int frog_(int num[], int n)
+{
+    vector<int> dp(n, 0);
+    dp[0] = 0;
+
+    for (int i = 1; i < n; i++)
+    {
+        int fs = dp[i - 1] + abs(num[i] - num[i - 1]);
+        int ss = INT_MAX;
+        if (i > 1)
+            ss = dp[i - 2] + abs(num[i] - num[i - 2]);
+
+        dp[i] = min(fs, ss);
+    }
+    return dp[n - 1];
+}
+int frog_space(int num[], int n)
+{
+    int prev = 0;
+    int prev2 = 0;
+    for (int i = 1; i < n; i++)
+    {
+        int left = prev + abs(num[i] - num[i - 1]);
+        int right = INT_MAX;
+        if (i > 1)
+        {
+            right = prev2 + abs(num[i] - num[i - 2]);
+        }
+        int curri = min(left, right);
+        prev2 = prev;
+        prev = curri;
+    }
+    return prev;
 }
 int main()
 {
@@ -23,5 +61,8 @@ int main()
     {
         cin >> num[i];
     }
-    cout << frog_jump(num, n - 1) << endl;
+    vector<int> dp(n + 1, -1);
+    cout << "Minimum distance is: " << frog_jump(num, n - 1, dp) << endl;
+    cout << "Minimum distance using tabulation: " << frog_(num, n) << endl;
+    cout << "Space optimisation: " << frog_space(num, n) << endl;
 }
